4fe75307bfa3b6ed78977b8e58415855ec6cb37a
ricardosilva <ricardo@we-rtek.com>
2024-02-15 12:55:32 -0300

    new version

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { Product } from '../../services/api/product';
 import { Subscription, debounceTime } from 'rxjs';
-import { ProductService } from '../../services/product.service';
 import { LayoutService } from '../../services/app.layout.service';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
@@ -35,6 +34,15 @@ export class DashboardComponent implements OnInit, OnDestroy {
   chartPotenciaBOptions: Highcharts.Options = {};
   chartPotenciaCOptions: Highcharts.Options = {};
   chartPotenciaTOptions: Highcharts.Options = {};
+  chartCorrenteAOptions: Highcharts.Options = {};
+  chartCorrenteBOptions: Highcharts.Options = {};
+  chartCorrenteCOptions: Highcharts.Options = {};
+  chartCorrenteTOptions: Highcharts.Options = {};
+  chartTensaoAOptions: Highcharts.Options = {};
+  chartTensaoBOptions: Highcharts.Options = {};
+  chartTensaoCOptions: Highcharts.Options = {};
+  chartConsumoOptions: Highcharts.Options = {};
+  chartGeracaoOptions: Highcharts.Options = {};
 
   items!: MenuItem[];
 
@@ -47,7 +55,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
     subscription!: Subscription;
 
     constructor(
-      private productService: ProductService,
       public layoutService: LayoutService,
       private readonly websocketService: WebSocketService,
       private readonly powerDataService: PowerDataService
@@ -55,34 +62,52 @@ export class DashboardComponent implements OnInit, OnDestroy {
         this.subscription = this.layoutService.configUpdate$
         .pipe(debounceTime(25))
         .subscribe((config) => {
-            this.initChart();
         });
     }
 
     ngOnInit() {
-        this.initChart();
-        this.productService.getProductsSmall().then(data => this.products = data);
-
-        this.items = [
-            { label: 'Add New', icon: 'pi pi-fw pi-plus' },
-            { label: 'Remove', icon: 'pi pi-fw pi-minus' }
-        ];
       this.websocketService.connectSocket('events');
       this.retrieveSumData();
       this.retieveNowData();
+      this.retrieveConsumoData();
+      this.retrieveGeracaoData();
       this.websocketService.receiveSocket().subscribe((receivedMessage: any) => {
         this.retrieveSumData();
+        this.retieveNowData();
+        this.retrieveConsumoData();
+        this.retrieveGeracaoData();
+      });
+    }
+
+    retrieveConsumoData() {
+      const startDay = moment.tz('America/Sao_Paulo').startOf('day').utc().toISOString();
+      const endDay = moment.tz('America/Sao_Paulo').utc().toISOString();
+      this.powerDataService.getConsumoData(startDay, endDay).subscribe((data: any) => {
+        this.chartConsumoOptions = this.powerDataService.getChartAreaOptions('Consumo de Energia [kWh]', 'kWh', 'darkred', data.map((d: any) => [moment.utc(d._time).valueOf(), d.ept_c]));
+      });
+    }
+
+    retrieveGeracaoData() {
+      const startDay = moment.tz('America/Sao_Paulo').startOf('day').utc().toISOString();
+      const endDay = moment.tz('America/Sao_Paulo').utc().toISOString();
+      this.powerDataService.getGeracaoData(startDay, endDay).subscribe((data: any) => {
+        this.chartGeracaoOptions = this.powerDataService.getChartAreaOptions('Geração de Energia [kWh]', 'kWh', 'darkgreen', data.map((d: any) => [moment.utc(d._time).valueOf(), d.ept_g]));
       });
     }
 
     retieveNowData() {
       this.powerDataService.getLastData().subscribe((data: any) => {
-        console.log('here', data);
         this.chartPotenciaAOptions = this.powerDataService.getGaugeOptions('Potência A', 'W', -2000, 2000, data.pa);
         this.chartPotenciaBOptions = this.powerDataService.getGaugeOptions('Potência B', 'W', -2000, 2000, data.pb);
         this.chartPotenciaCOptions = this.powerDataService.getGaugeOptions('Potência C', 'W', -2000, 2000, data.pc);
         this.chartPotenciaTOptions = this.powerDataService.getGaugeOptions('Potência Total', 'W', -2000, 2000, data.pt);
-
+        this.chartCorrenteAOptions = this.powerDataService.getGaugeOptions('Corrente A', 'A', 0, 100, data.iarms);
+        this.chartCorrenteBOptions = this.powerDataService.getGaugeOptions('Corrente B', 'A', 0, 100, data.ibrms);
+        this.chartCorrenteCOptions = this.powerDataService.getGaugeOptions('Corrente C', 'A', 0, 100, data.icrms);
+        this.chartCorrenteTOptions = this.powerDataService.getGaugeOptions('Corrente Total', 'A', 0, 100, data.itrms);
+        this.chartTensaoAOptions = this.powerDataService.getGaugeOptions('Tensão A', 'V', 0, 300, data.uarms);
+        this.chartTensaoBOptions = this.powerDataService.getGaugeOptions('Tensão B', 'V', 0, 300, data.ubrms);
+        this.chartTensaoCOptions = this.powerDataService.getGaugeOptions('Tensão C', 'V', 0, 300, data.ucrms);
       })
     }
 
@@ -106,69 +131,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
       });
     }
 
-    initChart() {
-        const documentStyle = getComputedStyle(document.documentElement);
-        const textColor = documentStyle.getPropertyValue('--text-color');
-        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
-
-        this.chartData = {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [
-                {
-                    label: 'First Dataset',
-                    data: [65, 59, 80, 81, 56, 55, 40],
-                    fill: false,
-                    backgroundColor: documentStyle.getPropertyValue('--bluegray-700'),
-                    borderColor: documentStyle.getPropertyValue('--bluegray-700'),
-                    tension: .4
-                },
-                {
-                    label: 'Second Dataset',
-                    data: [28, 48, 40, 19, 86, 27, 90],
-                    fill: false,
-                    backgroundColor: documentStyle.getPropertyValue('--green-600'),
-                    borderColor: documentStyle.getPropertyValue('--green-600'),
-                    tension: .4
-                }
-            ]
-        };
-
-        this.chartOptions = {
-            plugins: {
-                legend: {
-                    labels: {
-                        color: textColor
-                    }
-                }
-            },
-            scales: {
-                x: {
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder,
-                        drawBorder: false
-                    }
-                },
-                y: {
-                    ticks: {
-                        color: textColorSecondary
-                    },
-                    grid: {
-                        color: surfaceBorder,
-                        drawBorder: false
-                    }
-                }
-            }
-        };
-    }
-
     ngOnDestroy() {
-        if (this.subscription) {
-            this.subscription.unsubscribe();
-        }
         this.websocketService.disconnectSocket();
     }
 }
--- src/app/services/power-data.service.ts
+++ src/app/services/power-data.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/compat/database';
 import { InfluxDB } from '@influxdata/influxdb-client';
 import moment from 'moment-timezone';
 import { Observable } from 'rxjs';
@@ -9,8 +8,6 @@ import * as Highcharts from 'highcharts';
   providedIn: 'root'
 })
 export class PowerDataService {
-  private readonly allDataPath = '/user_identity';
-  private readonly basePath = '/user_identity/now';
 
   private readonly url = 'http://52.40.160.15:8086';
   private readonly token = 'ZgzULOOA4gARxR7mxs3qGEwpC_rUzZkunLaxPTcA6iTl1yWpu0Mob_CYxHKLCAFqUyZE8WfcjAnY9c73St_9Kg==';
@@ -19,60 +16,9 @@ export class PowerDataService {
   private client;
   private queryClient;
 
-  nowDataRef: AngularFireObject<any>;
-
-  constructor(
-    private readonly db: AngularFireDatabase
-  ) {
-    this.nowDataRef = db.object(this.basePath);
+  constructor() {
     this.client = new InfluxDB({ url: this.url, token: this.token });
     this.queryClient = this.client.getQueryApi(this.org);
-  }
-
-  getNowData() {
-    return this.nowDataRef;
-  }
-
-  getSumData() {
-    return this.db.object(this.getSumDataPath());
-  }
-
-  getSumMonthData() {
-    return this.db.object(this.getSumMonthDataPath());
-  }
-
-  getTodayListData() {
-    return this.db.list(this.getTodayListDataPath(), ref => ref.limitToLast(100));
-  }
-
-  resetAllData() {
-    this.db.object(this.allDataPath).remove();
-  }
-
-  getSumDataPath() {
-    const momentum = moment().tz('America/Sao_Paulo');
-    const year = momentum.format('YYYY');
-    const month = momentum.format('YYYY-MM');
-    const day = momentum.format('YYYY-MM-DD');
-
-    return `${this.allDataPath}/${year}/${month}/${day}/sum`;
-  }
-
-  getSumMonthDataPath() {
-    const momentum = moment().tz('America/Sao_Paulo');
-    const year = momentum.format('YYYY');
-    const month = momentum.format('YYYY-MM');
-
-    return `${this.allDataPath}/${year}/${month}/sum`;
-  }
-
-  getTodayListDataPath() {
-    const momentum = moment().tz('America/Sao_Paulo');
-    const year = momentum.format('YYYY');
-    const month = momentum.format('YYYY-MM');
-    const day = momentum.format('YYYY-MM-DD');
-
-    return `${this.allDataPath}/${year}/${month}/${day}`;
   }
 
   getPotenciaAtivaData(start: string, end: string): Observable<any> {
@@ -393,6 +339,44 @@ export class PowerDataService {
           valueSuffix: label
         }
       }]
+    };
+  }
+
+  getChartAreaOptions(title: string, label: string, color: string, data: any[]): Highcharts.Options {
+    return {
+      chart: {
+        type: 'area'
+      },
+      title: {
+        text: title
+      },
+      yAxis: {
+        title: {
+          text: label
+        }
+      },
+      xAxis: {
+        type: 'datetime',
+        title: {
+          text: 'Hora'
+        },
+        labels: {
+          format: '{value:%H:%M}'
+        }
+      },
+      time: {
+        timezone: 'America/Sao_Paulo'
+      },
+      series: [
+        {
+          data: data,
+          type: 'area',
+          color: color,
+        }
+      ],
+      legend: {
+        enabled: false
+      },
     };
   }
 }
